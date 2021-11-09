aa2b0d4cf12a3563f11cf33878cec5912e006519
Noah A Klem <noahaklem@gmail.com>
2021-11-09 16:32:49 -0700

    commit tree 8e456d2f03e66d115a3e63e796026a7b892d23a7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, name) {
+    return array.filter(nameMatch => 
+        nameMatch.toLowerCase() === name.toLowerCase()
+    )
+}
+
+function fuzzyMatch(array, partialName) {
+    return array.filter(partialMatch => 
+        partialMatch.toLowerCase().indexOf(partialName.toLowerCase()) === 0
+    )
+}
+
+function matchName(array, stringArg) {
+    return array.filter( object => 
+        object.name == stringArg
+    )
+}
